78578f9d876204587da9118fe3f5f403de6581e6
Shafiya57 <shafiya.begum75@gmail.com>
2022-09-11 19:38:13 +0530

    added dev branch

--- secondfile.java
+++ secondfile.java
@@ -1,1 +1,1 @@
-This file is to check the transfer of files from working area to staging area and to see how the remove command works.
+This file is to check the transfer of files from working area to staging area, to see how the remove command works and to also know how the branch command works.
